9254073caf6315da05c632076a204699935d0b35
Avneet Singh Malhotra <avneetmalhotra02@gmail.com>
2017-09-26 18:45:28 +0530

    jquery_on_exercise - submission 2

--- js/stack.js
+++ js/stack.js
@@ -1,55 +1,70 @@
 
-function DivsStack(selectors){
+function Stack(selectors){
   this.$stack = $(selectors.stack);
-  this.$addStackBtn = $(selectors.addStackBtn);
-  this.$sizeDiv = $(selectors.stackSizeDiv);
+  this.$pushBtn = $(selectors.pushBtn);
+  this.$stackSize = $(selectors.stackSize);
   this.stackSize = 0;
 }
 
-DivsStack.prototype.init = function(){
-  this.$addStackBtn.on('click', this.pushDiv());
-  this.$stack.on('click', 'div', this.divClicked());
+Stack.prototype.init = function(){
+  this.bindClickEventOnPushBtn();
+  this.bindClickEventOnStackElement(); 
 };
 
-DivsStack.prototype.pushDiv = function(){
+Stack.prototype.bindClickEventOnPushBtn = function(){
   var _this = this;
 
-  return function(){
-    _this.updateStackSize('push');
+  this.$pushBtn.on('click', function(){
+    _this.updateStackSize(true);
     //add div to top
     $('<div>Div no: ' + _this.stackSize + '</div>').prependTo(_this.$stack);
+  });
+};
+
+Stack.prototype.bindClickEventOnStackElement = function(){
+  var _this = this;
+
+  this.$stack.on('click', 'div', function(){
+    var $clickedElement = $(this);
+
+    //if last element is on the top(i.e: first in DOM), which is removed
+    if(_this.isFirstElement($clickedElement)){
+      $clickedElement.remove();
+      _this.updateStackSize(false)
+    }
+    //if not last element then
+    else{
+      if(!_this.isHighlighted($clickedElement))
+        $clickedElement.siblings('div').removeClass('highlight');
+    
+      $clickedElement.toggleClass('highlight');
+    }
+  });
+};
+
+Stack.prototype.isHighlighted = function($element){
+  return $element.hasClass('highlight');
+};
+
+Stack.prototype.isFirstElement = function($element){
+  return $element.is(this.$stack.children('div').first());
+};
+
+Stack.prototype.updateStackSize = function(increaseSize){
+  if(increaseSize){
+    this.stackSize++;
+    this.$stackSize.text('Stack Size: ' + this.stackSize);
+  }
+  else{
+    this.stackSize--;
+    this.$stackSize.text('Stack Size: ' + this.stackSize);
   }
 };
 
-DivsStack.prototype.divClicked = function(){
-  var _this = this;
-
-  return function(){
-    var $clickedDiv = $(this);
-
-    //last element is on the top, which is removed
-    if($clickedDiv.is(_this.$stack.children('div').first())){
-      $clickedDiv.remove();
-      _this.updateStackSize('pop')
-    }
-    else if(!$clickedDiv.hasClass('highlight'))
-      $clickedDiv.addClass('highlight').siblings('div').removeClass('highlight');
-    else
-      $clickedDiv.removeClass('highlight');
-  };
-};
-
-DivsStack.prototype.updateStackSize = function(action){
-  if(action === 'push')
-    this.$sizeDiv.text('Stack Size: ' + ++this.stackSize);
-  else if(action === 'pop')
-    this.$sizeDiv.text('Stack Size: ' + --this.stackSize);
-};
-
 $(document).ready(function(){
-  var divStack = new DivsStack({ 'addStackBtn' : '.add-stack',
-                                 'stackSizeDiv' : '.stack-size',
-                                 'stack' : '.stack'
-                                });
+  var divStack = new Stack({ 'pushBtn' : '.push',
+                             'stackSize' : '.stack-size',
+                             'stack' : '.stack'
+                            });
   divStack.init();
 });
